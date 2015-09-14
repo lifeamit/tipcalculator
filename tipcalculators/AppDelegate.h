54543dc3999d8059d1aabb7d000411da30203248
Amit Agarwal <amitagwl@yahoo-inc.com>
2015-09-13 21:32:56 -0700

    Initial Commit

---
+++ tipcalculators/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipcalculators
+//
+//  Created by Amit Agarwal on 9/13/15.
+//  Copyright (c) 2015 Amit Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipcalculators/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipcalculators
+//
+//  Created by Amit Agarwal on 9/13/15.
+//  Copyright (c) 2015 Amit Agarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
